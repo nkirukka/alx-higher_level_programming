b02a8ee45abab9ec2f6a2e67405a33fd76830867
Nkiruka Ebele <specialebele@gmail.com>
2022-05-31 19:05:53 +0000

    Update file 9 again

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return number % 10
+    last_d = 0
+    if number >= 0:
+        last_d = number % 10
+    else:
+        last_d = 10 - (number % 10)
+
+    print("{:d}".format(last_d), end="")
+    return last_d
