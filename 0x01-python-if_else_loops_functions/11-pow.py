8b26f8fcc189c7a450156c4a1d58a1fa08329c22
Nkiruka Ebele <specialebele@gmail.com>
2022-05-31 19:25:22 +0000

    Update file 11

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def pow(a, b):
+    ex = 0
+    if a >= 0 and b >= 0:
+        ex = a ** b
+    elif a < 0 and b > 0:
+        ex = (a) ** b
+    print("{:.2f}".format(ex))
+    return ex
