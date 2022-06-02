3eafc151c9e1735f47a4f6c846d01b66bdd18259
Nkiruka Ebele <specialebele@gmail.com>
2022-06-02 23:41:59 +0000

    Update file 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from hidden_4
+    import hidden_4
     sortedList = dir(hidden)
     for name in sortedList:
         if not name.startsWith("__"):
-            print("{:s}".format(name))
+            print(name)
